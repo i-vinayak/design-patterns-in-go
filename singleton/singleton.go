4dd0e571dbcf0a7d456979c6c7544a2fcb865b70
Vinayak <mulgundvinay@gmail.com>
2025-08-17 19:56:06 +0530

    multiple implementation for singleton

--- singleton/singleton.go
+++ singleton/singleton.go
@@ -1,6 +1,9 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+)
 
 type data struct {
 	message string
@@ -9,9 +12,43 @@ type data struct {
 var singletonData *data
 var once sync.Once
 
+// 1. Implementation using sync
 func NewSingleton(msg string) *data {
 	once.Do(func() {
+		fmt.Println("Creating instance")
 		singletonData = &data{message: msg}
 	})
 	return singletonData
 }
+
+// 2. Implementation using init
+// func init() {
+// 	if singletonData == nil {
+// 		singletonData = &data{}
+// 	}
+// }
+
+// func NewSingleton(msg string) *data {
+// 	if singletonData != nil {
+// 		fmt.Println("Singleton Already exists")
+// 	}
+// 	return singletonData
+// }
+
+// var lock sync.Mutex
+
+// 3. Implementation using Lock
+// func NewSingleton(msg string) *data {
+// 	if singletonData == nil {
+// 		lock.Lock()
+// 		defer lock.Unlock()
+// 		if singletonData == nil {
+// 			singletonData = &data{message: msg}
+// 		} else {
+// 			fmt.Println("Already exists")
+// 		}
+// 	} else {
+// 		fmt.Println("Instance exists")
+// 	}
+// 	return singletonData
+// }
